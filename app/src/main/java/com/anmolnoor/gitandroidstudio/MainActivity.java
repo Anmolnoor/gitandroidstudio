7a68ab9cdd7462ef0b6122735c12154d1a62cba3
Anmol Noor <anmolnoor59@gmail.com>
2020-04-08 01:29:31 +0530

    MainActivity and VCS Xml file Commited

--- app/src/main/java/com/anmolnoor/gitandroidstudio/MainActivity.java
+++ app/src/main/java/com/anmolnoor/gitandroidstudio/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("mytag","Message");
+        Log.i("mytag","Message");
     }
 }
